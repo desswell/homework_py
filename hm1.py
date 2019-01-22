119539e345fbd37761504d8c1a84e4392cd7f3b3
Алексей <kam@hitekki.ru>
2019-01-22 16:34:33 +0300

    homework1

--- hm1.py
+++ hm1.py
@@ -22,3 +22,45 @@ if a >= 18:
 else:
         print('Извините, пользование данным ресурсом только с 18 лет')
 
+#normal1
+
+a = int(input())
+m = 0
+
+while a > 0:
+        if a%10 > m:
+                m = a%10
+        a = a // 10
+print(m)
+
+#2
+a = int(input())
+b = int(input())
+
+a, b = b, a
+
+print(a, ' ', b)
+
+#3
+
+import math
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+d = b**2 - 4*a*c
+if d > 0:
+        x1 = (-b + math.sqrt(d)) / 2*a
+        x2 = (-b - math.sqrt(d)) / 2*a
+        print(x1, ' ', x2)
+elif d == 0:
+        print (-b / 2*a)
+elif d < 0:
+        print('Нет корней')
+
+#hard1
+a = float('inf')
+print(a==a**2)
+print(a==a*2)
+print (a > 999999)
